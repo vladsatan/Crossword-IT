43b5092f2608dbf3c24f7cfb240533c93e2d6290
ZadAndRot <kasirecivanna@gmail.com>
2022-08-17 22:38:53 +0300

    feat: :sparkles: Added background

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import './Container.css';
+import React from 'react'
+import './Container.css'
 
 export default function Container() {
-   
-    return (
-        <div className='container'>
-            <div className='container-header'>
-                <div className='imit-menu'>
-                <div className='red'></div>
-                <div className='yelloy'></div>
-                <div className='green'></div>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="container-header">
+        <div className="imit-menu">
+          <div className="red"></div>
+          <div className="yelloy"></div>
+          <div className="green"></div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
