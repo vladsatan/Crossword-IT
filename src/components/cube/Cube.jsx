afdb510fdc3f60830ecb0b1845922ae13ab57580
vladsatan <vladus007445@gmail.com>
2022-08-18 19:30:18 +0300

    добавил ограничения в поле ввода до 1 символа

--- src/components/cube/Cube.jsx
+++ src/components/cube/Cube.jsx
@@ -6,7 +6,7 @@ export default function Cube(props){
     const {id,number} = props
     return(
         <div id={id} className="cell">
-        <input type={'text'} className="cell_input"/>
+        <input type={'text'} maxLength={1} className="cell_input"/>
         <span className="number">{number}</span>
        </div>
     )
